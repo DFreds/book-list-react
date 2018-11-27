96b4c57d39ce540484654ed682ffd2068240d67e
Derek Fredrickson <fredricksond@upmc.edu>
2018-11-27 17:56:41 -0500

    Create books reducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,16 @@
+export default function() {
+    return [
+        {
+            title: ""
+        },
+        {
+            title: ""
+        },
+        {
+            title: ""
+        },
+        {
+            title: ""
+        }
+    ]
+}
