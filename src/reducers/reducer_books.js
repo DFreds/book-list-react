c970c721a8a915ae452d4e702293a11857a0bf9f
Derek Fredrickson <dfreds08@gmail.com>
2018-11-28 00:55:39 -0500

    Some progress on redux for books list

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,16 +1,16 @@
-export default function() {
-    return [
-        {
-            title: ""
-        },
-        {
-            title: ""
-        },
-        {
-            title: ""
-        },
-        {
-            title: ""
-        }
-    ]
+export default function () {
+  return [
+    {
+      title: "Javascript: The Good Parts"
+    },
+    {
+      title: "Harry Potter"
+    },
+    {
+      title: "The Dark Tower"
+    },
+    {
+      title: "Eloquent Ruby"
+    }
+  ];
 }
